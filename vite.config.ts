4169f2dc18b411467b81755e502911ab50ff17a0
yuya.is <6074132+yuya-dot-is@users.noreply.github.com>
2026-04-28 22:01:27 +0900

    ci: setup auto-deployment to github pages via github actions

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import fs from 'fs'
+
+
+
+export default defineConfig({
+    base: '/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                ...getSketchEntries()
+            },
+
+        },
+    },
+})
+
+// ルートディレクトリにある「00」から始まるディレクトリを取得
+function getSketchEntries() {
+    const entries: Record<string, string> = {}
+    const folders = fs.readdirSync(__dirname, { withFileTypes: true })
+        .filter(dirent => dirent.isDirectory() && dirent.name.startsWith('00'))
+        .map(dirent => dirent.name)
+    folders.forEach(folder => {
+        entries[folder] = resolve(__dirname, folder, 'index.html')
+    })
+    return entries
+}
