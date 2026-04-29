a409c3c9c8aaa1226f4b8095784785c3afcd60b8
yuya.is <6074132+yuya-dot-is@users.noreply.github.com>
2026-04-30 00:15:04 +0900

    006: separate vertices array to positions and colors

--- 006-cube/index.ts
+++ 006-cube/index.ts
@@ -1,43 +1,46 @@
 import { setupGPU, setupCanvas, setupVertexBuffer, createRenderPipelineDescriptor, createRenderPassDescriptor } from './setup';
 import shaderCode from './shader.wgsl?raw';
 
-const vertices = new Float32Array([
-     // 上の頂点
-    0.0, 0.6, 0, 1, // 座標
-    1, 0, 0, 1, // 色: 赤
-  
-    // 左下の頂点
-    -0.5, -0.6, 0, 1,
-    0, 1, 0, 1, // 色: 緑
-  
-    // 右下の頂点
-    0.5, -0.6, 0, 1, 
-    0, 0, 1, 1, // 色: 青
+// 頂点座標
+const positions = new Float32Array([
+   0.0,  0.6, 0.0, 1.0, // 頂点1
+  -0.5, -0.6, 0.0, 1.0, // 頂点2
+   0.5, -0.6, 0.0, 1.0  // 頂点3
+]);
+
+// 頂点の色
+const colors = new Float32Array([
+  1.0, 0.0, 0.0, 1.0, // 赤
+  0.0, 1.0, 0.0, 1.0, // 緑
+  0.0, 0.0, 1.0, 1.0  // 青
 ]);
 
 const clearColor = { r: 0.0, g: 0.5, b: 1.0, a: 1.0 };
 
-const vertexBufferLayouts: GPUVertexBufferLayout[]  = [{
-        attributes: [
-            {
-                shaderLocation: 0, // 位置
-                offset: 0,
-                format: "float32x4",
-            },
-            {
-                shaderLocation: 1, // 色
-                offset: 16,
-                format: "float32x4",
-            },
-        ],
-        arrayStride: 32,
-        stepMode: "vertex",
-}];
+const vertexBufferLayouts: GPUVertexBufferLayout[]  = [
+    {
+        arrayStride: 16, // float32 * 4 = 4 bytes * 4 = 16 bytes
+        attributes: [{
+            shaderLocation: 0, // WGSL @location(0)
+            offset: 0,
+            format: "float32x4",
+        }],
+    },
+    {
+        arrayStride: 16, // float32 * 4 = 4 bytes * 4 = 16 bytes
+        attributes: [{
+            shaderLocation: 1, // WGSL @location(1)
+            offset: 0,
+            format: "float32x4",
+        }],
+    }
+];
 
 const main = async () => {
     const { gpu, device } = await setupGPU();
     const { context } = setupCanvas(gpu, device);
-    const vertexBuffer = setupVertexBuffer(device, vertices);
+    const posBuffer = setupVertexBuffer(device, positions);
+    const colorBuffer = setupVertexBuffer(device, colors);
     const shaderModule = device.createShaderModule({ code: shaderCode });
     const renderPipelineDescriptor = createRenderPipelineDescriptor(gpu, shaderModule, vertexBufferLayouts);
     const renderPipeline = device.createRenderPipeline(renderPipelineDescriptor);
@@ -45,7 +48,8 @@ const main = async () => {
     const commandEncoder = device.createCommandEncoder();
     const passEncoder: GPURenderPassEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
     passEncoder.setPipeline(renderPipeline);
-    passEncoder.setVertexBuffer(0, vertexBuffer);
+    passEncoder.setVertexBuffer(0, posBuffer);
+    passEncoder.setVertexBuffer(1, colorBuffer);
     passEncoder.draw(3);
     passEncoder.end();
     device.queue.submit([commandEncoder.finish()]);
