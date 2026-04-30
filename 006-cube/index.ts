b212944effcf5581457dfdce5313677d321f8ce7
yuya.is <6074132+yuya-dot-is@users.noreply.github.com>
2026-04-30 12:01:28 +0900

    006: add TODO comment for grasp mvp matrix detail

--- 006-cube/index.ts
+++ 006-cube/index.ts
@@ -7,16 +7,19 @@ import { mat4 } from 'wgpu-matrix';
 const createMvpMatrix = (context: GPUCanvasContext, dist: Float32Array) => {
     // Projection Matrix (WebGPUの 0~1 Z-rangeに自動対応)
     const aspect = context.canvas.width / context.canvas.height;
+    // TODO: 行列の内容をコメントで記載
     const projection = mat4.perspective(Math.PI / 4, aspect, 0.1, 100);
 
     // View Matrix (カメラの位置)
+    // TODO: 行列の内容をコメントで記載
     const view = mat4.lookAt(
         [0, 0, 5], // カメラの位置 (eye)
         [0, 0, 0], // 注視点 (target)
         [0, 5, 0]  // 上方向 (up)
     );
 
-    // 3. Model Matrix (回転)
+    // Model Matrix (回転)
+    // TODO: 行列の内容をコメントで記載
     const model = mat4.rotationY(performance.now() / 1000);
 
     // MVPを統合 (計算順序は右から左: P * V * M)
