fbb8e620ecda841d46c564a362d8d9368e3ff495
yuya.is <6074132+yuya-dot-is@users.noreply.github.com>
2026-05-02 16:39:54 +0900

    008: add position data

--- 008-texture-mapping/data.ts
+++ 008-texture-mapping/data.ts
@@ -32,6 +32,40 @@ export const vertices = new Float32Array([
     ...[ 0.5, -0.5, -0.5,  1.0], ...[ 1.0,  0.0,  0.0], ...[1.0, 0.0, 1.0, 1.0], // 23: 奥下
 ]);
 
+// 頂点 position(4) + normal(3) + color(4) = 7 floats
+export const positions = new Float32Array([
+    // 手前
+    ...[-0.5,  0.5,  0.5,  1.0], //  0: 左上
+    ...[ 0.5,  0.5,  0.5,  1.0], //  1: 右上
+    ...[-0.5, -0.5,  0.5,  1.0], //  2: 左下
+    ...[ 0.5, -0.5,  0.5,  1.0], //  3: 右下
+     // 奥
+    ...[-0.5,  0.5, -0.5,  1.0], //  4: 左上
+    ...[ 0.5,  0.5, -0.5,  1.0], //  5: 右上
+    ...[-0.5, -0.5, -0.5,  1.0], //  6: 左下
+    ...[ 0.5, -0.5, -0.5,  1.0], //  7: 右下
+    // 上
+    ...[-0.5,  0.5,  0.5,  1.0], //  8: 左手前
+    ...[ 0.5,  0.5,  0.5,  1.0], //  9: 右手前
+    ...[-0.5,  0.5, -0.5,  1.0], // 10: 左奥
+    ...[ 0.5,  0.5, -0.5,  1.0], // 11: 右奥
+    // 下
+    ...[-0.5, -0.5,  0.5,  1.0], // 12: 左手前
+    ...[ 0.5, -0.5,  0.5,  1.0], // 13: 右手前
+    ...[-0.5, -0.5, -0.5,  1.0], // 14: 左奥
+    ...[ 0.5, -0.5, -0.5,  1.0], // 15: 右奥
+    // 左
+    ...[-0.5, -0.5,  0.5,  1.0], // 17: 手前下
+    ...[-0.5,  0.5,  0.5,  1.0], // 16: 手前上
+    ...[-0.5,  0.5, -0.5,  1.0], // 18: 奥上
+    ...[-0.5, -0.5, -0.5,  1.0], // 19: 奥下
+    // 右
+    ...[ 0.5,  0.5,  0.5,  1.0], // 20: 手前上
+    ...[ 0.5, -0.5,  0.5,  1.0], // 21: 手前下
+    ...[ 0.5,  0.5, -0.5,  1.0], // 22: 奥上
+    ...[ 0.5, -0.5, -0.5,  1.0], // 23: 奥下
+]);
+
 // 頂点の組み合わせ
 export const indexes = new Uint32Array([
      0,  1,  2, // 手前1
@@ -70,3 +104,47 @@ export const vertexBufferLayouts: GPUVertexBufferLayout[]  = [
         ],
 	},
 ];
+
+const computeNormal = (indexes: Uint32Array, pos: Float32Array, dist?: Float32Array) => {
+    const EPSILON = 1e-10;
+    const out = dist || new Float32Array(pos.length);
+    for(let i = 0; i < indexes.length; i += 3) {
+        // 三角形の3つの頂点のindex
+        const idxA = indexes[i    ] * 4;
+        const idxB = indexes[i + 1] * 4;
+        const idxC = indexes[i + 2] * 4;
+        // 三角形の2辺のベクトル
+        const p1x = pos[idxA    ] - pos[idxC    ];
+        const p1y = pos[idxA + 1] - pos[idxC + 1];
+        const p1z = pos[idxA + 2] - pos[idxC + 2];
+        const p2x = pos[idxB    ] - pos[idxC    ];
+        const p2y = pos[idxB + 1] - pos[idxC + 1];
+        const p2z = pos[idxB + 2] - pos[idxC + 2];
+        // 外積（三角形の垂線）
+        let nx = p1y * p2z - p1z * p2y;
+        let ny = p1z * p2x - p1x * p2z;
+        let nz = p1x * p2y - p1y * p2x;
+        // ノルムを計算
+        const squaredNorm = nx * nx + ny * ny + nz * nz;
+        if(squaredNorm > EPSILON) {
+            // 外積の正規化
+            const invNorm = 1.0 / Math.sqrt(squaredNorm)
+            nx *= invNorm;
+            ny *= invNorm;
+            nz *= invNorm;
+        }
+        // 結果の配列に格納
+        out[idxA    ] = nx;
+        out[idxA + 1] = ny;
+        out[idxA + 2] = nz;
+        out[idxB    ] = nx;
+        out[idxB + 1] = ny;
+        out[idxB + 2] = nz;
+        out[idxC    ] = nx;
+        out[idxC + 1] = ny;
+        out[idxC + 2] = nz;
+    }
+    return out;
+}
+
+console.log(computeNormal(indexes, positions))
